3526b87e0069225247f8c04ac414ec00dfd2e6d2
DimaMirk <dima.myruk@gmail.com>
2021-11-19 14:11:52 +0200

    fixed problem with saving

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,15 +3,19 @@ const throttle = require('lodash.throttle');
 const refs = {
   form: document.querySelector('.feedback-form'),
   texarea: document.querySelector('textarea'),
+  email: document.querySelector('input'),
 };
 
 const MESSEGE_KEY = 'messege';
+const EMAIL_KEY = 'email';
 messegeToTextarea();
+emailToTEmail();
 
 const formData = {};
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.texarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.email.addEventListener('input', throttle(onEmailInput, 500));
 
 refs.form.addEventListener('input', e => {
   formData[e.target.name] = e.target.value;
@@ -21,6 +25,7 @@ function onFormSubmit(evt) {
   evt.preventDefault();
   evt.currentTarget.reset();
   localStorage.removeItem(MESSEGE_KEY);
+  localStorage.removeItem(EMAIL_KEY);
   console.log(formData);
 }
 
@@ -28,10 +33,20 @@ function onTextareaInput(evn) {
   const massege = evn.target.value;
   localStorage.setItem(MESSEGE_KEY, massege);
 }
+function onEmailInput(evn) {
+  const email = evn.target.value;
+  localStorage.setItem(EMAIL_KEY, email);
+}
 
 function messegeToTextarea() {
   const sevedMessege = localStorage.getItem(MESSEGE_KEY);
   if (sevedMessege) {
     refs.texarea.value = sevedMessege;
+  }
+}
+function emailToTEmail() {
+  const sevedEmail = localStorage.getItem(EMAIL_KEY);
+  if (sevedEmail) {
+    refs.email.value = sevedEmail;
   }
 }
